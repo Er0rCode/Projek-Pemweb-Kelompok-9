1a522043ffad7897cc128a3f1b11500e062f6b5c
nurhusain123 <nur554322@gmail.com>
2025-05-05 12:50:07 +0700

    nambah header, footer ( husen)

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,29 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mekar Sari</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Mekar Sari</title>
+  
+  <link rel="stylesheet" href="<?= base_url('assets/header.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/footer.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/home.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/about.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/layanan.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/produk.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/kontak.css'); ?>">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container">
-    <a class="navbar-brand fw-bold" href="<?= base_url(); ?>">MS</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="<?= base_url(); ?>">Beranda</a></li>
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('about'); ?>">Tentang</a></li>
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('produk'); ?>">Produk</a></li>
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('layanan'); ?>">Layanan</a></li>
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('kontak'); ?>">Kontak</a></li>
-      </ul>
-    </div>
+<nav>
+  <div class="logo">
+    <img src="assets/images/logo3.png" alt="Logo">
   </div>
+  <ul>
+  <li class="nav-item"><a class="nav-link" href="<?= base_url(); ?>">Beranda</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?= base_url('about'); ?>">Tentang</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?= base_url('layanan'); ?>">Layanan</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?= base_url('produk'); ?>">Produk</a></li>
+  <li class="nav-item"><a class="nav-link" href="<?= base_url('kontak'); ?>">Kontak</a></li>
+
+  </ul>
 </nav>
-<div style="margin-top: 80px;">
+
+
+<script src="<?= base_url('assets/js/home.js'); ?>"></script>
+
+
 </body>
-</html>
