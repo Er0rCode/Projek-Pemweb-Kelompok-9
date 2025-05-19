241fe6d2027190a9a4e9073730096090c8d3ee91
nurhusain123 <nur554322@gmail.com>
2025-05-19 14:38:41 +0700

    nambahin best seller

--- application/views/home.php
+++ application/views/home.php
@@ -6,3 +6,29 @@
   </div>
   <img src="assets/images/rumah1.png" alt="Rumah">
 </section>
+
+<section class="best-seller">
+  <h2 class="section-title">Barang Best Seller</h2>
+  <div class="product-grid">
+    <div class="product-card">
+      <img src="assets/images/semenputih.png" alt="Semen Tiga Roda">
+      <h3>Semen Tiga Roda</h3>
+      <p>Rp64.000</p>
+    </div>
+    <div class="product-card">
+      <img src="assets/images/semengresik.png" alt="Semen Gresik">
+      <h3>Semen Gresik</h3>
+      <p>Rp60.000</p>
+    </div>
+    <div class="product-card">
+      <img src="assets/images/pasirhitam.png" alt="Pasir">
+      <h3>Pasir</h3>
+      <p>Rp843.000</p>
+    </div>
+    <div class="product-card">
+      <img src="assets/images/batubata.png" alt="Batu Bata">
+      <h3>Batu Bata</h3>
+      <p>Rp600</p>
+    </div>
+  </div>
+</section>
